827975c11bc4f198230bb2a25c88c1829d7bdddb
unknown <vitomartinsalmeida@gmail.com>
2022-04-01 16:54:40 -0300

    metade da aula

--- index.php
+++ index.php
@@ -6,7 +6,17 @@
 
 	$app->get('/postagens', function() {
 
-		echo "Lista Postagens";
+		echo 'postagens';
+
+	} );
+
+	$app->get('/usuarios/{id}', function($request, $response) {
+
+		$id = $request->getAttribute('id');
+
+		//Verificar se ID é válido no DB
+
+		echo "Usuario é $id";
 
 	} );
 
