29dd2e8b214b37702305da94f831fabe9b49c279
unknown <vitomartinsalmeida@gmail.com>
2022-04-04 15:20:09 -0300

    criando variaveis nas rotas

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 
 	$app = new \Slim\App;
 
-	$app->get('/postagens', function() {
+	$app->get('/postagem', function() {
 
-		echo 'postagens';
+		echo 'postagem';
 
 	} );
 
-	$app->get('/usuarios/{id}', function($request, $response) {
+	$app->get('/user/{id}', function($request, $response) {
 
 		$id = $request->getAttribute('id');
 
@@ -19,6 +19,56 @@
 		echo "Usuario é $id";
 
 	} );
+
+	$app->get('/', function() {
+
+		echo "Index";
+
+	});
+
+	//O COLCHETE É USADO PARA TORNAR UM PARAMETRO OPCIONAL
+
+	$app->get('/users[/{id}]', function($request, $response) {
+
+		$id = $request->getAttribute('id');
+
+		echo '{
+				{"nome" : "Jamilton"},
+				{"nome" : "Raul"},
+				{"nome" : "Vitor"},
+				{"nome" : "Antonio"}
+			}';
+		echo '<br> <hr> <br>';
+		echo  "O id do usuário é: $id";
+
+	});
+
+	$app->get('/postagens[/{mes}[/{ano}]]', function($request, $response) {
+
+		$mes = $request->getAttribute('mes');
+
+		$ano = $request->getAttribute('ano');
+
+		if(isset($mes) && isset($ano)) {
+			echo "Postagens do mes $mes, ano $ano";
+		} else {
+			echo "Postagens";
+		}
+
+	});
+
+	//os 2 pontos são utilizados para definir o que é aceito dentro desses itens (o asterisco significa qualquer valor)
+	$app->get('/lista/{itens:.*}', function($request, $response) {
+
+		$itens = $request->getAttribute('itens');
+
+		echo "<pre>";
+		var_dump(explode("/", $itens));
+		echo "</pre>";	
+
+	});
+
+	/* Nomear Rotas */
 
 	$app->run();
 
